370d147f0f64daf74dc4bab088a7274b8b758056
Broderick Daniel <broderickdaniel0@gmail.com>
2019-01-09 11:09:55 -0800

    Completed the module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[3].ToLower();
+            var type = parts[2].ToLower();
             //Converts parts[3] into a boolean (bool.Parse).
             var weighted = bool.Parse(parts[3]);
             BaseGradeBook gradeBook;
