7fc829947d7c561eb6ec1f5390a9faf51f43e6b8
Broderick Daniel <broderickdaniel0@gmail.com>
2019-01-09 09:33:24 -0800

    Update
    Commited Changes...

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+   public class StandardGradeBook : BaseGradeBook
+    {
+        // :base(name) allows us to access the parameter in the BaseGradeBook class.
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
